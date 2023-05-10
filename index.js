7a6421c9e630d48532d757ed4c2a4d457c35c1db
tbohn2 <tanner.bohn@yahoo.com>
2023-05-10 15:23:23 -0700

    Created addEmployee() with prompts

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const startSelect = () =>
                 console.log('All employees');
             }
             if (selection.selection1 === 'Add Employee') {
-                console.log('hi');
+                addEmployee();
             }
             if (selection.selection1 === 'Update Employee Role') {
                 console.log('hi');
@@ -60,7 +60,61 @@ function viewAllEmployees() {
             console.log('oh no!');
         }
         console.table(result)
+        startSelect();
     });
 }
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the first name of the employee? '
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the last name of the employee? '
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'What is the role of the employee? '
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: 'Who is the manager of the employee? ',
+                choices: ['Michael Phelps', 'Bonnie Lead', 'Berenice Ankind', 'Aiwanna Dacash']
+            },
+        ])
+        .then((selection) => {
+            db.query(`SELECT first_name, last_name FROM employee WHERE manager_id IS NULL;`, [], (err, results) => {
+                for (let i = 0; i < results.length; i++) {
+                    if (results[i].first_name + " " + results[i].last_name === selection.manager) {
+                        console.log("yo it worked for" + selection.manager);
+                    }
+                }
+                startSelect()
+            })
+
+            //         const sql = `SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
+            // FROM employee e 
+            // JOIN role ON e.role_id = role.id
+            // JOIN department ON role.department_id = department.id
+            // LEFT JOIN employee m ON e.manager_id = m.id;`;
+            //         const params = [];
+            //         db.query(sql, params, (err, result) => {
+            //             if (err) {
+            //                 console.log('oh no!');
+            //             }
+            //             console.table(result)
+            //         });
+            //         startSelect();
+        })
+}
+
+
 
 startSelect();

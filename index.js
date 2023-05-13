a6870b4fdbd72d9b8f569292ff291f217bbf40e7
tbohn2 <tanner.bohn@yahoo.com>
2023-05-13 16:00:12 -0700

    Made addEmployee function an Employee class method

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const startSelect = () =>
                 display.viewAll()
             }
             if (selection.selection1 === 'Add Employee') {
-                addEmployee();
+                const display = new Employee
+                display.addNew()
             }
             if (selection.selection1 === 'Update Employee Role') {
                 console.log('hi');
@@ -75,6 +76,65 @@ class Employee extends Workplace {
             const params = [];
             this.view(sql, params)
         }
+
+        this.addNew = () => {
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'firstName',
+                        message: 'What is the first name of the employee? '
+                    },
+                    {
+                        type: 'input',
+                        name: 'lastName',
+                        message: 'What is the last name of the employee? '
+                    },
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: 'What is the role of the employee? ',
+                        choices: ['Head Doctor', 'Affiliate Doctor', 'Lead Assistant', 'Assistant', 'Sterile Tech', 'Front Lead', 'Treatment Coordinator', 'Receptionist', 'Auditor', 'Insurance Verification']
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: 'Who is the manager of the employee? ',
+                        choices: ['Michael Phelps', 'Bonnie Lead', 'Berenice Ankind', 'Aiwanna Dacash']
+                    },
+                ])
+                .then((selection) => {
+                    console.log(selection.role);
+                    const fName = selection.firstName
+                    const lName = selection.lastName
+                    const mFirstName = selection.manager.split(' ')[0]
+                    const mLastName = selection.manager.split(' ')[1]
+                    let mID
+                    let roleID
+                    findID()
+                    function findID() {
+                        db.query(`SELECT id FROM role WHERE title = ?;`, selection.role, (err, results) => {
+                            if (err) { console.log(err); }
+                            roleID = results[0].id;
+                        })
+                        db.query(`SELECT id FROM employee WHERE first_name = ? AND last_name = ?;`, [mFirstName, mLastName], (err, results) => {
+                            if (err) { console.log(err); }
+                            mID = results[0].id;
+                            insertEmployee(fName, lName, roleID, mID)
+                        })
+                    }
+                    function insertEmployee(fName, lName, roleID, mID) {
+                        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+                            VALUES ('${fName}', '${lName}', ${roleID}, ${mID})`
+                        const params = []
+                        console.log('New employee added');
+                        db.query(sql, params, (err, results) => {
+                            if (err) { console.log(err); }
+                            startSelect()
+                        })
+                    }
+                })
+        }
     }
 }
 
@@ -102,110 +162,4 @@ class Department extends Workplace {
     }
 }
 
-// employee ids, first names, last names, job titles, departments, salaries, and managers
-function viewAllEmployees() {
-    const sql = `SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
-    FROM employee e 
-    JOIN role ON e.role_id = role.id
-    JOIN department ON role.department_id = department.id
-    LEFT JOIN employee m ON e.manager_id = m.id;`;
-    const params = [];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            console.log('oh no!');
-        }
-        console.table(result)
-        startSelect();
-    });
-}
-
-function addEmployee() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'firstName',
-                message: 'What is the first name of the employee? '
-            },
-            {
-                type: 'input',
-                name: 'lastName',
-                message: 'What is the last name of the employee? '
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: 'What is the role of the employee? ',
-                choices: ['Head Doctor', 'Affiliate Doctor', 'Lead Assistant', 'Assistant', 'Sterile Tech', 'Front Lead', 'Treatment Coordinator', 'Receptionist', 'Auditor', 'Insurance Verification']
-            },
-            {
-                type: 'list',
-                name: 'manager',
-                message: 'Who is the manager of the employee? ',
-                choices: ['Michael Phelps', 'Bonnie Lead', 'Berenice Ankind', 'Aiwanna Dacash']
-            },
-        ])
-        .then((selection) => {
-            const fName = selection.firstName
-            const lName = selection.lastName
-            let mFirst
-            let mLast
-            let mID
-            let roleID
-            findID()
-            function findID() {
-                db.query(`SELECT * FROM role;`, [], (err, results) => {
-                    for (let i = 0; i < results.length; i++) {
-                        if (results[i].title === selection.role) {
-                            roleID = results[i].id;
-                            console.log(roleID);
-                        }
-                    }
-                })
-                db.query(`SELECT first_name, last_name FROM employee WHERE manager_id IS NULL;`, [], (err, results) => {
-                    for (let i = 0; i < results.length; i++) {
-                        if (results[i].first_name + " " + results[i].last_name === selection.manager) {
-                            mFirst = results[i].first_name;
-                            mLast = results[i].last_name;
-                        }
-                    }
-                    db.query(`SELECT id FROM employee WHERE first_name = '${mFirst}' AND last_name = '${mLast}';`, [], (err, results) => {
-                        mID = results[0].id;
-                        console.log(mID);
-                        insertEmployee()
-                    })
-                })
-            }
-            function insertEmployee() {
-                const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-                    VALUES ('${fName}', '${lName}', ${roleID}, ${mID})`
-                console.log(sql);
-                db.query(sql, [], (err, results) => {
-                    console.log(results);
-                })
-            }
-        })
-
-    // startSelect()
-
-    //         const sql = `SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
-    // FROM employee e 
-    // JOIN role ON e.role_id = role.id
-    // JOIN department ON role.department_id = department.id
-    // LEFT JOIN employee m ON e.manager_id = m.id;`;
-    //         const params = [];
-    //         db.query(sql, params, (err, result) => {
-    //             if (err) {
-    //                 console.log('oh no!');
-    //             }
-    //             console.table(result)
-    //         });
-    //         startSelect();
-
-
-
-}
-
-
-
-startSelect();
+startSelect()
